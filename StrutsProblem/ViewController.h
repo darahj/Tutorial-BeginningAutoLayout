cf4ada70c4e8f49f733478b2fc65a0234dfc1a51
Darah <darah.joseph@gmail.com>
2015-08-15 14:06:17 -0400

    Initial Commit

---
+++ StrutsProblem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StrutsProblem
+//
+//  Created by Darah on 2015-08-15.
+//  Copyright (c) 2015 Darah Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
